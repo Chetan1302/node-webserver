cccca2edea87e517886ad1ff467069019ed54f65
Raskar <chetan.raskar@gilbarco.com>
2019-01-18 00:54:46 -0800

    resolved runtime error of environment variable for PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //Assigning Expresss object to app  which will be used throughout
 var app = express();
